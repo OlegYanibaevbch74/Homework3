83a0d0f4e82a5c952b70966a7eaffa75a8edd489
Олег Яныбайевич <davletoleg@yandex.ru>
2022-12-06 19:30:53 +0500

    Начальный коммит 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog=8;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        var paper=763789;
+
+
+
+
+
+
+
     }
 }

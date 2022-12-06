ecf7a41870067a97b79d7d78a068e0b3b068251a
Олег Яныбайевич <davletoleg@yandex.ru>
2022-12-06 19:37:58 +0500

    Начальный коммит 1.4 получилось!!! (замена 8 на 8,0 и алгоритм работает)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var dog=8;
+        var dog=8.0;
         System.out.println(dog);
         var cat=3.6;
         System.out.println(cat);
